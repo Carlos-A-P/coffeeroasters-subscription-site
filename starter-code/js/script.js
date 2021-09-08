6b77365e77dcf8e2f0f7ade4e7604803d13fb707
Carlos-A-P <pcarlos491@gmail.com>
2021-09-08 15:52:32 -0700

    started the javascript interactive components on buttons and accordion

--- starter-code/js/script.js
+++ starter-code/js/script.js
@@ -1,9 +1,51 @@
 const openNav = document.getElementById('open-nav')
 const closeNav = document.getElementById('close-nav')
 const mobileNav = document.getElementById('mobile_menu')
+const modal = document.getElementById('modal')
 
 function toggleNav() {
     openNav.classList.toggle('hidden')
     closeNav.classList.toggle('hidden')
     mobileNav.classList.toggle('hidden')
+    // window.removeEventListener('scroll', noScroll);
+    scroll()
+console.log(mobileNav.classList.contains('hidden'))    
 }
+
+function scroll() {
+    disableScroll()
+    enableScroll()
+}
+
+const accOptions = document.querySelectorAll('[data-options]')
+
+accOptions.forEach(acc =>{
+    acc.addEventListener('click', () => {
+        const listOptions = acc.parentElement.lastElementChild
+        // console.log(listOptions)
+        listOptions.classList.toggle('hidden')
+    })
+})
+
+function togglePlan() {
+    modal.classList.toggle('hidden')
+}
+
+function disableScroll() {
+    // window.addEventListener('scroll', noscroll)
+    
+    window.onscroll = () => {
+        window.scrollTo(0,0)
+    }
+}
+
+function enableScroll() {
+    if (mobileNav.classList.contains('hidden')){
+        window.onscroll = function() {}
+    }
+    
+    if (modal.classList.contains('hidden')){
+        window.onscroll = function() {}
+    }
+}   
+
