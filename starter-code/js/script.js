ea3b146fcd957b91e3bae44d70ecbdcd7e69e0d1
Carlos-A-P <pcarlos491@gmail.com>
2021-09-10 12:34:25 -0700

    I got the modal to get the updated summary for both different summarys
    The only thing that is left is to make sure the button stays disable from switching between coffees and adding the updated balance

--- starter-code/js/script.js
+++ starter-code/js/script.js
@@ -3,7 +3,9 @@ const closeNav = document.getElementById('close-nav')
 const mobileNav = document.getElementById('mobile_menu')
 const modal = document.getElementById('modal')
 const accOptions = document.querySelectorAll('[data-options]')
+const createPlan = document.querySelector('[data-button]')
 
+// toggles mobile meny
 function toggleNav() {
     openNav.classList.toggle('hidden')
     closeNav.classList.toggle('hidden')
@@ -12,28 +14,30 @@ function toggleNav() {
     scroll()
 }
 
+// disables/enables scroll for mobile nav and checkout 
 function scroll(x) {
     disableScroll()
-    enableScroll(x)
-    console.log(x)     
+    enableScroll(x)   
 }
 
 
-
+// ppens up accordion menu for each
 accOptions.forEach(acc =>{
     acc.addEventListener('click', () => {
         const listOptions = acc.parentElement.lastElementChild
-        // console.log(listOptions)
         listOptions.classList.toggle('hidden')
         acc.parentElement.firstElementChild.classList.toggle('open')
     })
 })
 
+// shows modal
 function togglePlan() {
     modal.classList.toggle('hidden')
     scroll('_modal')
+    insertChoices()
 }
 
+// disables scroll
 function disableScroll() {
     
     window.onscroll = () => {
@@ -41,6 +45,7 @@ function disableScroll() {
     }
 }
 
+// enables scroll
 function enableScroll(y) {
     if(y === '_modal'){
         if (modal.classList.contains('hidden')){
@@ -50,35 +55,30 @@ function enableScroll(y) {
         if (mobileNav.classList.contains('hidden')){
             window.onscroll = function() {}
         }        
-    }
-    console.log(y) 
+    } 
 }   
 
-
+// adds a function so that user can return back from checkout
 const order_summary = document.getElementById('order_sum')
 order_summary.addEventListener('click', ()=> {
     togglePlan()
+    
 })
 
-// const option = document.getElementsByClassName('card').firstElementChild
-// const cardSelected = document.querySelectorAll('card')
 const card = document.querySelectorAll('[data-card]')
 const summary = document.getElementById('summary')
-
+const grindOption = document.getElementById('grindOption')
+let modalSummary = document.getElementById('modalSummary')
+let choice1, choice2, choice3, choice4, choice5
+let result
+// adds options for end summary at the press of the button
 card.forEach(selected => {
     selected.addEventListener('click', () => {
-        console.log(selected.firstElementChild.innerText)
-        let acc1, acc2, acc3, acc4, acc5, sum1, sum2, sum3, sum4, sum5, choice1, choice2, choice3, choice4, choice5 
-        sum1 = summary.childNodes[1]
-        sum2 = summary.childNodes[3]
-        sum3 = summary.childNodes[5]
-        sum4 = summary.childNodes[7]
-        sum5 = summary.childNodes[9]
         let choice = selected.firstElementChild.innerText
-        if (choice === 'Capsule' || choice === 'Filter' || choice === 'Expresso') {
+        console.log(choice)
+        if (choice === 'Capsule' || choice === 'Filter' || choice === 'Espresso') {
             choice1 = choice
-        }
-        if (choice === 'Single Origin' || choice === 'Decaf' || choice === 'Blended') {
+        }else if (choice === 'Single origin' || choice === 'Decaf' || choice === 'Blended') {
             choice2 = choice
         }
         if (choice === '250g' || choice === '500g' || choice === '1000g') {
@@ -90,85 +90,57 @@ card.forEach(selected => {
         if (choice === 'Every week' || choice === 'Every 2 weeks' || choice === 'Every month') {
             choice5 = choice
         }
+
+        arr = [choice1, choice2, choice3, choice4, choice5]
+        if(choice1 === 'Capsule'){
+            grindOption.classList.add('disabled')
+            capsule()
+        }else{
+            for(let i=0; i < arr.length; i++){
+                if(arr[i] === undefined){
+                    arr[i] = `<span></span>`
+                } else{
+                    arr[i] = `<span class="full">${arr[i]}</span>`
+                }
+            } 
+            grindOption.classList.remove('disabled')
+            
+            summary.innerHTML = `“I drink my coffee as ${arr[0]}, with a ${arr[1]} type of bean. ${arr[2]} ground ala ${arr[3]}, sent to me ${arr[4]}.”`
+            result = summary.innerHTML
+
+            if(choice1 !== undefined && choice2 !== undefined && choice3 !== undefined && choice4 !== undefined && choice5 !== undefined) {
+            createPlan.removeAttribute('disabled')
+        }
+        }
+
         
-        switch(choice1) {
-            case 'Capsule':
-                acc1 = choice1
-                sum1.classList.add('full')
-                sum1.innerText = acc1
-            case 'Filter':
-                acc1 = choice1
-                sum1.classList.add('full')
-                sum1.innerText = acc1
-            case 'Espresso':
-                acc1 = choice1
-                sum1.classList.add('full')
-                sum1.innerText = acc1
-        }
-        switch (choice2){
-            case 'Single Origin':
-                acc2 = choice2
-                sum2.classList.add('full')
-                sum2.innerText = acc2
-            case 'Decaf':
-                acc2 = choice2
-                sum2.classList.add('full')
-                sum2.innerText = acc2
-            case 'Blended':
-                acc2 = choice2
-                sum2.classList.add('full')
-                sum2.innerText = acc2
-            default: ''
-        }
-        switch (choice3){
-            case '250g':
-                acc3 = choice3
-                sum3.classList.add('full')
-                sum3.innerText = acc3
-            case '500g':
-                acc3 = choice3
-                sum3.classList.add('full')
-                sum3.innerText = acc3
-            case '1000g':
-                acc3 = choice3
-                sum3.classList.add('full')
-                sum3.innerText = acc3
-            default: ''
-        }
-        switch (choice4){
-            case 'Wholebean':
-                acc4 = choice4
-                sum4.classList.add('full')
-                sum4.innerText = acc4
-            case 'Filter':
-                acc4 = choice4
-                sum4.classList.add('full')
-                sum4.innerText = acc4
-            case 'Cafetiére':
-                acc4 = choice4
-                sum4.classList.add('full')
-                sum4.innerText = acc4
-            default: ''
-        }
-        switch (choice5){
-            case 'Every week':
-                acc5 = choice5
-                sum5.classList.add('full')
-                sum5.innerText = acc5
-            case 'Every 2 weeks':
-                acc5 = choice5
-                sum5.classList.add('full')
-                sum5.innerText = acc5
-            case 'Every month':
-                acc5 = choice5
-                sum5.classList.add('full')
-                sum5.innerText = acc5
-            default: ''
-        }
+        return arr, result
     })
 })
 
-// console.log(summary.childNodes[1].innerText)
-console.log(summary.childNodes[3])
+function capsule() {
+    for(let i=1; i < arr.length; i++){
+        if(arr[i] === undefined){
+            arr[i] = `<span></span>`
+        } else{
+            arr[i] = `<span class="full">${arr[i]}</span>`
+        }
+    }
+    summary.innerHTML = `“I drink my coffee using <span class="full">${choice1}</span>, with a ${arr[1]} type of bean. ${arr[2]}, sent to me ${arr[4]}.”`
+    result = summary.innerHTML
+    if(choice1 !== undefined && choice2 !== undefined && choice4 !== undefined && choice5 !== undefined) {
+        createPlan.removeAttribute('disabled')
+    }
+}
 
-// summary.childNodes[1].innerText = 'cheese'
+// this inserts the summary into the modal when the button is clicked
+let arr
+function insertChoices(){
+    // let j = 0
+
+    // for(let i=1; i <= 9 ; i = i+2 ){
+    //     modalSummary.childNodes[i].innerText = arr[j]
+    //     j++
+    // }
+    modalSummary.innerHTML = result
+}
